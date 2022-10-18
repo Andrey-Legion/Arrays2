da63dc907a6c01cdb8aa166213796c687b0ba6a9
Andrey-Legion <andrey.legion@gmail.com>
2022-10-18 23:49:04 +0300

    ДЗ Массивы Часть2 (Editing)

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
-//import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Task1");
+
         int[] arrMonth = {110000, 150000, 120000, 170000, 100000, 140000, 150000, 200000, 180000, 130000, 130000, 190000, 200000,
                 110000, 100000, 140000, 170000, 120000, 100000, 180000, 160000, 190000, 200000, 130000, 120000, 150000, 100000, 200000, 170000, 120000};
         int sumMonth = 0;
@@ -13,7 +14,9 @@ public class Main {
             }
         }
         System.out.println("Сумма трат за месяц составила " + sumMonth + " рублей");
+
         System.out.println("Task2");
+
             //Если траты до 200000
             int maxMonth = 0, minMonth = 210000;
             for (int i = 0; i < arrMonth.length; i++) {
